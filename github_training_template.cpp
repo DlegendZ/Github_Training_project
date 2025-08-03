dfac69cd8a50db0fb95381fc64b824b8cd195d33
DlegendZ <raynaldarvanlim@gmail.com>
2025-08-03 17:50:18 +0800

    resolving merge conflicts

--- github_training_template.cpp
+++ github_training_template.cpp
@@ -1,11 +1,8 @@
 #include <stdio.h>
 
 int main() {
-<<<<<<< HEAD
     printf("This is the first changes in the clone\n");
-=======
     printf("Github Training Template\n");
     printf("Changes from the main.\n");
->>>>>>> main
     return 0;
 }   
